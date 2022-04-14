68f603f59ef880959f4be4c55da2395f7353dac9
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-04-14 10:50:50 -0400

    simple clock arithmetic

---
+++ beginnerClock/main.js
@@ -0,0 +1,28 @@
+/*
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+3.6 x 106
+*/
+
+function past(h, m, s){
+    return milliSecs = (h * (3.6* Math.pow(10,6))) + (m * 60000) + (s * 1000)
+}
+
+console.log(past(0,1,1),61000)
+console.log(past(1,1,1),3661000)
+console.log(past(0,0,0),0)
+console.log(past(1,0,1),3601000)
+console.log(past(1,0,0),3600000)
