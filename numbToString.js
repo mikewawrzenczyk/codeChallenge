44f0b5cd003e1aa216bf5e7fc04903bd91608ad4
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-05-14 15:21:19 -0400

    easily make a number a string program

---
+++ numbToString.js
@@ -0,0 +1,19 @@
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+Examples:
+
+123 --> "123"
+999 --> "999"
+
+*/
+
+// function numberToString(num) {
+//     // Return a string of the number here!
+//     return num+''
+//   }
+
+let numberToString = num => num+''
+
+console.log(numberToString(67), '67');
