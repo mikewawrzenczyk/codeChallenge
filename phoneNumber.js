63b36a172586ebd05f5aa8e526008634bf9d3f75
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-07-17 22:54:12 -0400

    phone nmumber program from array

--- phoneNumber.js
+++ phoneNumber.js
@@ -19,6 +19,17 @@ function createPhoneNumber(numbers){
     return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${phoneNumString.slice(3)}`
 }
 
+/* different solution
+var format = "(xxx) xxx-xxxx";
+
+for(var i = 0; i < numbers.length; i++)
+{
+format = format.replace('x', numbers[i]);
+}
+
+return format;
+*/
+
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
