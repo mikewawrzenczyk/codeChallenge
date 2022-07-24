8a4eb6f1fec585c2668e83c2cc6aa78a2512e033
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-07-24 16:20:30 -0400

    sort only odd numbers in an array and leave even numbers alone program

--- sortTheOdd.js
+++ sortTheOdd.js
@@ -10,27 +10,18 @@ Examples
 */
 
 function sortArray(array) {
-    //get the odds into an array
-    //sort odds array
-    //put in each sorted odd number from odd array into original array but replace each odd, leave each even
-    const oddsOnly = array.filter(value => value % 2 !=0).sort((a,b) => a-b)
-    // return oddsOnly
-    // console.log(oddsOnly)
-    let returningArray =[]
-    for(let i=0;i<oddsOnly.length;i++){
-        for(let j=0;j<array.length;j++){
-            if(array[j] % 2 !=0){
-                // console.log(oddsOnly[i])
-                array[j] = oddsOnly[i]
-                // j++
-            }
-        }
-    }
+/*
+Here's a solution using mostly the built-in array methods. Get a list of just the odds, sort it, then map through the original, replacing each item with the first sorted odd if the item is odd, or itself if even:
+*/
 
+    const odds = array
+        .filter(x => x%2)
+        .sort((a, b) => a - b);
+        
     return array
-
-
+        .map(x => x%2 ? odds.shift() : x);
 }
+
 
 console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
 console.log(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
