0ac9dd269cd1561ab61a11741d6339a07ec73daf
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-07-31 11:16:00 -0400

    converting values to strings

---
+++ booleanToString.js
@@ -0,0 +1,13 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+function booleanToString(b){
+    //your code here
+    return String(b)
+}
+
+
+console.log(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+console.log(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
