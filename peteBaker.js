60450f7d68e4c1e8127cffa439b70cac7167189c
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-08-02 21:56:53 -0400

    baking math problem program

--- peteBaker.js
+++ peteBaker.js
@@ -12,48 +12,18 @@ cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flo
 
 */
 function cakes(recipe, available) {
-    let cakesCounter= 0
-    let recipeCounter = []
-    let ingredientCounter = 0
-    // console.log(Object.keys(available))
-    // console.log(Object.values(available))
-    //iterate through both objects nesting
-    for(const [availableKey, availableValue] of Object.entries(recipe)){
-        for(const [recipeKey, recipeValue] of Object.entries(available)){
-            //if the value pairs match in each object then check to see if they can be divided
-            // console.log(recipeKey)
-            // console.log(availableKey)
-            if(availableKey === recipeKey){
-                //divide 
-                if(recipeValue / availableValue > 1){
-                    console.log(recipeKey)
-            console.log(availableKey)
-                    ingredientCounter++
-                    // console.log(availableValue)
-                    // console.log(recipeValue)
-                    // console.log(availableValue / recipeValue)
-                    // recipeCounter.push(Math.round(availableValue / recipeValue))
-                    recipeCounter.push(Math.floor(recipeValue/availableValue))
-                }
-                // else{
-                //     return 0
-                // }
-            }
-        }
+    let maximumCakes = []
+    for(const item in recipe){
+      let numberOfCakes = 0
+      while(available[item] >= recipe[item]){
+        let temp = available[item] - recipe[item]
+        available[item] = temp
+        numberOfCakes++
+      }
+      maximumCakes.push(numberOfCakes)
     }
-    console.log(recipeCounter)
-    console.log(`Recipe keys that match ${recipeCounter.length} and value length is ${Object.keys(available).length}`)
-    // console.log(`available ${available}`)
-    // console.log(`recipe ${recipe}`)
-    if(Object.keys(available).length < Object.keys(recipe).length)
-        return 0
-    
-    recipeCounter.sort((a,b) => a-b)
-    return recipeCounter[0]
-    // console.log(ingredientCounter)
-    
-    // return cakesCounter
-}
+    return(Math.min(...maximumCakes))
+  }
 
 let recipe = {flour: 500, sugar: 200, eggs: 1};
 let available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
@@ -78,6 +48,6 @@ available = {flour: 4,
     butter: 2,
     nuts: 20,
     chocolate: 9,
-    cocoa: 15,
-    crumbles: 4};
+    crumbles: 4,
+    cocoa: 15,};
 console.log(cakes(recipe, available), 0);
