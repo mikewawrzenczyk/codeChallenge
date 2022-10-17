2d7b30c52ea5dc456e13304293309a3e34458fec
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-10-17 15:26:10 -0400

    simple look combining a string through an array

---
+++ countSheep.js
@@ -0,0 +1,20 @@
+/*
+If you can't sleep, just count sheep!!
+Task:
+
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+*/
+
+var countSheep = function (num){
+    let sheepArray = []
+    for(let i=1; i<=num;i++){
+        sheepArray.push(`${i} sheep...`)
+    }
+    return sheepArray.join('')
+}
+
+console.log(countSheep(0), "");
+console.log(countSheep(1), "1 sheep...");
+console.log(countSheep(2), "1 sheep...2 sheep...");
+console.log(countSheep(3), "1 sheep...2 sheep...3 sheep...");
