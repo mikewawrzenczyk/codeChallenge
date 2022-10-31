06560e47b50076d8f20d6551c8c325c9f4ec9512
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-10-31 18:25:55 -0400

    string transform problem

---
+++ challenging/stringTransformer.js
@@ -0,0 +1,29 @@
+/*
+Description:
+
+Given a string, return a new string that has transformed based on the input:
+
+    Change case of every character, ie. lower case to upper case, upper case to lower case.
+    Reverse the order of words from the input.
+
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+
+You may assume the input only contain English alphabet and spaces.
+
+*/
+
+function transformStr(str){
+    //split str " " ->  reverse -> join
+    //split str " " -> map l === l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()
+    let reversedStr = str.split(' ').reverse().join(' ')
+    let transformedStr = reversedStr.split('').map(l => l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()).join(' ')
+    //this arrow function in map has an implicit return
+    return transformedStr
+}
+
+console.log(transformStr("Example Input"), "iNPUT eXAMPLE")
+console.log(transformStr("Mike Dubs"), "mIKE dUBS")
