583c6f877d74110bc3b445f9441773f968d68942
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-11-02 23:22:09 -0400

    sorting by last character of each string prep

---
+++ challenging/sortByLastChar.js
@@ -0,0 +1,20 @@
+/*
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+
+last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+last('take me to semynak'), ['take', 'me', 'semynak', 'to'] 
+*/
+
+function last(x){
+    //
+}
+
+console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']); 
+console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to']);  
+
