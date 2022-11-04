d2dfd65f010bdbadde35e3b516d131ef881f38d8
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-11-03 21:07:40 -0400

    finished sorting by last letter of string

--- challenging/sortByLastChar.js
+++ challenging/sortByLastChar.js
@@ -11,9 +11,11 @@ last('take me to semynak'), ['take', 'me', 'semynak', 'to']
 */
 
 function last(x){
+    //split sort
+    //sort -> grab last letter of word using slice
     //
+    return x.split(' ').sort((a,b) => a.charCodeAt(a.length -1) - b.charCodeAt(b.length -1))
 }
-
 console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
 console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']); 
 console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to']);  
