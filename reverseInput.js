b54ddb975c55a314c135c212e62ecb7444cbfc30
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-12-04 11:28:38 -0500

    refactor reverse input to a one line

--- reverseInput.js
+++ reverseInput.js
@@ -4,13 +4,20 @@ loop through string adding current value to previous equals a reversed string
 */
 
 function reverseInput(string){
-    let reversed = ''
-    for(let i=string.length-1;i>=0;i--){
-        reversed += string[i]
-    }
-    return reversed
+    // let reversed = ''
+    // for(let i=string.length-1;i>=0;i--){
+    //     reversed += string[i]
+    // }
+    // return reversed
+    // for(let char of string){
+    //     reversed = char + reversed
+    // }
+    // return reversed
+    //better one liner
+    return string.split('').reduce((acc,c) =>c + acc, '')
 }
 
 console.log(reverseInput('mike'), 'ekim')
 console.log(reverseInput('anna'), 'anna')
 console.log(reverseInput('equal'), 'lauqe')
+console.log(reverseInput('john'), 'nhoj')
