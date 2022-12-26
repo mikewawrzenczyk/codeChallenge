5d71e8c257a47b36ddcfb46a1534743b363b14a1
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-12-26 17:50:51 -0500

    remove lowercase vowels in string function

---
+++ vowelRemover.js
@@ -0,0 +1,21 @@
+/*
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+Examples
+
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+
+    don't worry about uppercase vowels
+    y is not a vowel
+*/
+
+function shortcut(string){
+    return string.split('').filter(element => element != "a" && element != "e" && element !="i" && element !="o" && element !="u").join('')
+}
+
+console.log(shortcut('hello'), 'hll')
+console.log(shortcut('how are you today?'), 'hw r y tdy?')
+console.log(shortcut('complain'), 'cmpln')
+console.log(shortcut('never'), 'nvr')
