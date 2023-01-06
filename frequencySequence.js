fa9213a608a80a49f3135be1dc6943720ccedfe8
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-01-06 18:17:41 -0500

    refactor to one liner

--- frequencySequence.js
+++ frequencySequence.js
@@ -9,27 +9,32 @@ Example (s, sep --> Output)
 */
 
 function freqSeq(s, sep){
-    let counts = {}
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
-        // If the character is not yet in the counts object, set its count to 1
-        if (!counts[char]) {
-          counts[char] = 1;
-        } else {
-          // Otherwise, increment the count for that character
-          counts[char]++;
-        }
-      }
-    //   console.log(counts)
-    let returnArray = []
-    for(let i=0; i < s.length;i++){
-        // console.log(s[i])
-        if(counts.hasOwnProperty(s[i])){
-            // console.log(counts[i])
-            returnArray.push(counts[s[i]])
-        }
-    }
-    return returnArray.join(sep)
+  
+    //one liner with methods
+    return str.split('').map((v, i, arr) => arr.filter(vi => vi === v).length).join(sep);
+  
+  
+    // let counts = {}
+    // for (let i = 0; i < s.length; i++) {
+    //     let char = s[i];
+    //     // If the character is not yet in the counts object, set its count to 1
+    //     if (!counts[char]) {
+    //       counts[char] = 1;
+    //     } else {
+    //       // Otherwise, increment the count for that character
+    //       counts[char]++;
+    //     }
+    //   }
+    // //   console.log(counts)
+    // let returnArray = []
+    // for(let i=0; i < s.length;i++){
+    //     // console.log(s[i])
+    //     if(counts.hasOwnProperty(s[i])){
+    //         // console.log(counts[i])
+    //         returnArray.push(counts[s[i]])
+    //     }
+    // }
+    // return returnArray.join(sep)
 }
 
 console.log(freqSeq('hello world', '-'), '1-1-3-3-2-1-1-2-1-3-1');
