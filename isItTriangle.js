03f89e74c19ee7d61e8bedb76d3da7ea85ec3e9b
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-01-08 18:49:51 -0500

    checking for triangles function

--- isItTriangle.js
+++ isItTriangle.js
@@ -7,9 +7,6 @@ Implement a function that accepts 3 integer values a, b, c. The function should 
 
 function isTriangle(a,b,c)
 {
-//    if (a+b > c) && (a+c > b) && (c+b > a){
-//     return true
-//    }
     return (a+b > c) && (a+c > b) && (c+b > a)
 }
 
