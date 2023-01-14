eb38ce3afaafd40a47345f734d375a69abe10474
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-01-14 16:19:55 -0500

    fix a function problem

---
+++ functionFixer.js
@@ -0,0 +1,11 @@
+/*
+Fix this function
+*/
+function addFive(num) {
+    var total = num + 5
+    return total
+}
+
+console.log(addFive(5), 10)
+console.log(addFive(0), 5)
+console.log(addFive(-5), 0)
