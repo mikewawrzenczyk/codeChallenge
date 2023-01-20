0d7daa710eb027be2ae390d98afe991fae63d381
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-01-19 19:02:19 -0500

    string manipulation program

---
+++ nameShuffler.js
@@ -0,0 +1,18 @@
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+
+
+*/
+
+function nameShuffler(str){
+    //Shuffle It
+    return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler('john McClane'),'McClane john');
+console.log(nameShuffler('Mary jeggins'),'jeggins Mary')
+console.log(nameShuffler('tom jerry'),'jerry tom')
