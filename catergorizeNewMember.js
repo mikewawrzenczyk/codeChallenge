1af51bf730757b22a8903b7ea39b4c1b9a0da4ca
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-01-22 20:33:36 -0500

    refactor to use map

--- catergorizeNewMember.js
+++ catergorizeNewMember.js
@@ -17,15 +17,21 @@ output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 function openOrSenior(data){
   //senior 55 years old handicap > 7
   //array pair is age and handicap
-  let dataString = []
-  for(let i=0; i < data.length; i++){
-    if(data[i][0] >= 55 && data[i][1] > 7)
-        dataString.push('Senior')
-    else    
-        dataString.push('Open')
-  }
-  return dataString
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open')
+  // let dataString = []
+  // for(let i=0; i < data.length; i++){
+  //   if(data[i][0] >= 55 && data[i][1] > 7)
+  //       dataString.push('Senior')
+  //   else    
+  //       dataString.push('Open')
+  // }
+  // return dataString
 }
+
+//one line solution with map
+//return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+// Destructuring: [age, handicap] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+// Arrow Functions: () => {} https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
 console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]),['Open', 'Senior', 'Open', 'Senior'])
 console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]),['Open', 'Open', 'Open', 'Open'])
