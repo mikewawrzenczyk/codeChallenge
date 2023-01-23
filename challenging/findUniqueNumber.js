656a86d4851e3090a96357881deae6c028d5481d
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-01-23 10:19:53 -0500

    findin a unique number based on occurence using hashmap

---
+++ challenging/findUniqueNumber.js
@@ -0,0 +1,38 @@
+/*
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+*/
+function findUniq(arr) {
+
+
+    let hashmap = {},
+    count = 0,
+    maxChar = null
+
+    //create hashmap
+    for(let letter of arr){
+    hashmap[letter] = (hashmap[letter] || 0) + 1
+    }
+
+    for(const value in hashmap){
+        // console.log(value)
+        if(hashmap[value] === 1){
+            return value
+        }
+    }
+
+    
+}
+
+console.log(findUniq([ 1, 0, 0 ]), 1);
+console.log(findUniq([ 0, 1, 0 ]), 1);
+console.log(findUniq([ 0, 0, 1 ]), 1);
+console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]), 2);
+console.log(findUniq([ 1, 1, 2, 1, 1 ]), 2);
+console.log(findUniq([ 3, 10, 3, 3, 3 ]), 10);
