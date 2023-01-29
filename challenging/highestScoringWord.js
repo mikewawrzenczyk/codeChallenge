88d33ce9c3391e9e7a0c6124c28b6204b9fa12b1
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-01-28 22:24:21 -0500

    refactor with better solution

--- challenging/highestScoringWord.js
+++ challenging/highestScoringWord.js
@@ -32,6 +32,11 @@ function high(x){
     return highScoreWord
 }
 
+/*better solution
+  let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+*/
+
 console.log(high('man i need a taxi up to ubud'), 'taxi');
 console.log(high('what time are we climbing up the volcano'), 'volcano'); 
 console.log(high('take me to semynak'), 'semynak');   
