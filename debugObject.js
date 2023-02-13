c21d2ebae9bf8153d8645c4091994e9c23124e54
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-02-13 18:53:40 -0500

    debug problem, syntax issue

---
+++ debugObject.js
@@ -0,0 +1,17 @@
+/*
+Debug this to return object
+
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return
+    results;
+}
+*/
+
+function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return results
+  }
+
