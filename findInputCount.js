c441605be2dfbd5ac4b3b8891c5257eaf0155e0b
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-02-20 12:28:44 -0500

    using a string split method refactor

--- findInputCount.js
+++ findInputCount.js
@@ -18,12 +18,13 @@ Notes
 */
 
 function strCount(str, letter){  
-  let count = 0
-  for(let i = 0; i < str.length; i++){
-    if (str[i] === letter)
-        count++
-  }
-  return count
+//   let count = 0
+//   for(let i = 0; i < str.length; i++){
+//     if (str[i] === letter)
+//         count++
+//   }
+//   return count
+    return str.split(letter).length-1
 }
 
 console.log(strCount('Hello', 'o'), 1);
