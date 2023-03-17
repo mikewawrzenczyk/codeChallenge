15984ddd60eba2cfb6ffdcb2de549cdeac539865
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-03-17 17:38:22 -0400

    refactored with comments

--- challenging/zeroesToEnd.js
+++ challenging/zeroesToEnd.js
@@ -6,22 +6,26 @@ moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 
 function moveZeros(arr) {
-//   for(let i =0; i<arr.length;i++){
-//     if(arr[i] === 0){
-//         arr.splice(i,1)
-//         arr.push(0)
-//     }
-//   }
-//   return arr
-    if(arr[0]===0){
-        arr.push(arr.splice(i, 1).pop())
+  let arrayLength = arr.length
+  let zeroCount = 0
+  for(let i =0; i<arrayLength;i++){
+    if(arr[i] === 0){
+        arr.splice(i,1)
     }
-    for(let i=arr.length;i>0;i--){
-        if(arr[i]===0){
-            arr.push(arr.splice(i,1).pop())
-        }
-    }
-    return arr
+  }
+
+  //if you go forward in array and remove an element you skip the element after the one removed because the index gets moved to the left.
+
+  return arr
+    // if(arr[0]===0){
+    //     arr.push(arr.splice(i, 1).pop())
+    // }
+    // for(let i=arr.length;i>0;i--){
+    //     if(arr[i]===0){
+    //         arr.push(arr.splice(i,1).pop())
+    //     }
+    // }
+    // return arr
 
 }
 
