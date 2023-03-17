6bb4df1bed042296c969cd58d3bfba7b2ab4a5ae
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-03-16 23:53:10 -0400

    refactor with a solution using sort

--- challenging/findUniqueNumber.js
+++ challenging/findUniqueNumber.js
@@ -7,29 +7,34 @@ findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
 It’s guaranteed that array contains at least 3 numbers.
 
 The tests contain some very huge arrays, so think about performance.
+
 */
 function findUniq(arr) {
-
-
-    let hashmap = {},
-    count = 0,
-    maxChar = null
+    let hashmap = {}
+    /*
+    hashmap = {
+        1: 1,
+        0: 2
+    }
+    */
 
     //create hashmap
-    for(let letter of arr){
-    hashmap[letter] = (hashmap[letter] || 0) + 1
+    for(let number of arr){
+        //1, 0, 0 
+        hashmap[number] = (hashmap[number] || 0) + 1
     }
-
+    
     for(const value in hashmap){
         // console.log(value)
         if(hashmap[value] === 1){
             return value
         }
     }
-
-    
 }
-
+/* solution by using sort!
+  arr.sort((a,b)=>a-b);
+  return arr[0]==arr[1]?arr.pop():arr[0]
+*/
 console.log(findUniq([ 1, 0, 0 ]), 1);
 console.log(findUniq([ 0, 1, 0 ]), 1);
 console.log(findUniq([ 0, 0, 1 ]), 1);
