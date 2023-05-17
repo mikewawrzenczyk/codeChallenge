8d954acbfaf9609881f7447f5385b0afce7def1e
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-05-16 22:16:15 -0400

    refactored solution with concat and sort

--- mergeArrays.js
+++ mergeArrays.js
@@ -16,7 +16,8 @@ Happy coding!
 */
 
 function mergeArrays(arr1, arr2) {
-  return [... new Set([...arr1,...arr2])]
+  // return [... new Set([...arr1,...arr2])]
+  return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))))
 }
 console.log(mergeArrays([1,2,3,4], [5,6,7,8]), [1,2,3,4,5,6,7,8], "Basic tests");
 console.log(mergeArrays([1,3,5,7,9], [10,8,6,4,2]), [1,2,3,4,5,6,7,8,9,10], "Basic tests");
