e5f75da1ace4ccc7fffc2f1a5821dd659d758721
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-05-23 23:49:20 -0400

    solution explained in program

--- return5.js
+++ return5.js
@@ -2,6 +2,8 @@
 Write a function that always returns 5
 
 Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Solution: the trick was to just use a string and return the length, this way a number or math etc. was not used.
 */
 
 function unusualFive() {
