e338dc47123307d5402edce8674c6ef94e921d1b
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-06-08 12:58:41 -0400

    array manilpulation

---
+++ addValue.js
@@ -0,0 +1,12 @@
+/*
+Add the value "stuff" to the websites array.
+After your code executes the websites array should == ["stuff"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+*/
+
+var websites = []
+websites.push('stuff')
