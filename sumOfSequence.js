7609e22d47abe137ae62d389b0e60de7007ab488
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2023-06-23 19:02:49 -0400

    refactor

--- sumOfSequence.js
+++ sumOfSequence.js
@@ -18,7 +18,7 @@ const sequenceSum = (begin, end, step) => {
   // May the Force be with you
   let sum =0
   if (begin > end) return 0
-  for(let i=begin; i<=end; i+= step){
+  for(let i=begin; i<=end; i+=step){
     sum+=i
   }
   return sum
