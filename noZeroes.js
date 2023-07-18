a0ece58b274f417755c9a7340ed76ec3cee35b39
Mike Wawrzenczyk <57688647+mikewawrzenczyk@users.noreply.github.com>
2023-07-17 21:53:08 -0400

    no trailing zeroes program

---
+++ noZeroes.js
@@ -0,0 +1,27 @@
+/*
+Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+
+Zero alone is fine, don't worry about it. Poor guy anyway
+
+*/
+
+function noBoringZeros(n) {
+  // your code
+  return Number(n.toString().replace(/0+$/, ''));
+}
+
+console.log(noBoringZeros(1450), 145)
+console.log(noBoringZeros(960000), 96)
+console.log(noBoringZeros(1050), 105)
+console.log(noBoringZeros(-1050), -105)
+console.log(noBoringZeros(-105), -105)
+console.log(noBoringZeros(0), 0)
